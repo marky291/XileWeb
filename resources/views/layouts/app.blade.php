6cf2de8bbfcfe6e63099da8b1321dda5c56a300e
Mark Hester <Marky360@live.ie>
2020-11-13 10:40:24 +0000

    Messenger integration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -141,5 +141,31 @@
             </div>
         </footer>
     </div>
+    <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v9.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="100309695047046"
+  theme_color="#13cf13"
+  logged_in_greeting="Hello! 👋  I'm here to help, let us know if you have any questions!"
+  logged_out_greeting="Hello! 👋  I'm here to help, let us know if you have any questions!">
+      </div>
 </body>
 </html>
