d38bbff0d4ec1c660b036cc38ab1853122986a7c
Mark Hester <Marky360@live.ie>
2020-11-13 10:44:54 +0000

    dont defer JS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Styles -->
     <link href="{{ mix('assets/core.css') }}" rel="stylesheet">
