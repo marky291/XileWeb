3bc1657feab616969034f652d2c2d7350e5de424
Mark Hester <Marky360@live.ie>
2020-12-04 15:35:09 +0000

    Download array_key_first

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -36,7 +36,7 @@
                         <a href="#steps2play" class="w-full no-underline hidden lg:block">
                             <button class="flex items-center w-full mt-4 text-left btn btn-primary">
                                 <svg class="w-4 h-4 mr-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
-                                <span>{{ config('downloads.full')[array_key_last(config('downloads.full'))]['name'] }}</span>
+                                <span>{{ config('downloads.full')[array_key_first(config('downloads.full'))]['name'] }}</span>
                             </button>
                         </a>
                     </div>
