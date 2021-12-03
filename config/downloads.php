dbb63eca8f9c1e954554245c48487d4cf97f224f
Mitsune <50927576+MitsuneBewbs@users.noreply.github.com>
2021-12-02 18:27:08 -0700

    Updated PC Downloads, Snow Patches 02-12-2021

--- config/downloads.php
+++ config/downloads.php
@@ -5,18 +5,18 @@ return [
 
     'lite' => [
         [
-            'name' => 'Download Lite from Google Drive (1.31gb)',
-            'link' => 'https://drive.google.com/file/d/12R2I58doWU5lQi9tal9HrJDciwNZzNWp/view?usp=sharing',
+            'name' => 'Download Lite from Google Drive (1.32gb)',
+            'link' => 'https://drive.google.com/file/d/1jo0RvAsBfsB_DAc0R3lF-lGtpdAhLX9X/view?usp=sharing',
             'bttn' => 'btn-secondary',
         ],
         [
-            'name' => 'Download Lite from Mega Upload (1.31gb)',
-            'link' => 'https://mega.nz/file/rCIyATaC#4cdWdNzgbl1S_rsq5ulI80SuOB3AVickn5UAy6XdGFQ',
+            'name' => 'Download Lite from Mega Upload (1.32gb)',
+            'link' => 'https://mega.nz/file/Ta4iHT4Y#8h3B10k0MmoNLKDbRLjju-r2aP7_nOhK7h6gOx70iyE',
             'bttn' => 'btn-secondary',
         ],
         [
-            'name' => 'Download Lite from Mediafire (1.31gb)',
-            'link' => 'https://www.mediafire.com/file/gjgavl1qm1dlkd2/Play-Lite.zip/file',
+            'name' => 'Download Lite from Mediafire (1.32gb)',
+            'link' => 'https://www.mediafire.com/file/mxlgfbzhe521g3d/Play-Lite.zip/file',
             'bttn' => 'btn-secondary',
         ],
     ],
@@ -24,17 +24,17 @@ return [
     'full' => [
         [
             'name' => 'Download Full from Google Drive (2.77gb)',
-            'link' => 'https://drive.google.com/file/d/1UhNYr320D25IV47XftrWiDVBm8o8QxZh/view?usp=sharing',
+            'link' => 'https://drive.google.com/file/d/1ql4vDqqban0XFlLY-aOyL3ZRhtyOiYn9/view?usp=sharing',
             'bttn' => 'btn-primary',
         ],
         [
             'name' => 'Download Full from Mega Upload (2.77gb)',
-            'link' => 'https://mega.nz/file/XSBAUBpB#LPs3NW4zdyg-beLzR-Ss1_8ckxIGkZhLtgvudmM-25U',
+            'link' => 'https://mega.nz/file/ba4yjRID#pSOTiKmSXZ-2y5BGUulQ6bXVidWiWH6syc_iEWyIzco',
             'bttn' => 'btn-secondary',
         ],
         [
             'name' => 'Download Full from Mediafire (2.77gb)',
-            'link' => 'https://www.mediafire.com/file/qdz2m133jcdkjgu/Play.zip/file',
+            'link' => 'https://www.mediafire.com/file/6xjo6q4w6wnrjd8/Play.zip/file',
             'bttn' => 'btn-secondary',
         ],
     ],
