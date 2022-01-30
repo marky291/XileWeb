92503ba2c3e21adb9c78b6e9f3b4659174035149
Mitsune <50927576+MitsuneBewbs@users.noreply.github.com>
2022-01-30 15:07:14 -0700

    Fresh Downloads 30-01-2022

--- config/downloads.php
+++ config/downloads.php
@@ -6,17 +6,17 @@ return [
     'lite' => [
         [
             'name' => 'Download Lite from Google Drive (1.70gb)',
-            'link' => 'https://drive.google.com/file/d/1RK5t8U3_capIoKiztqjJdKHGpzBfT1ik/view?usp=sharing',
+            'link' => 'https://drive.google.com/file/d/1CIzmL_HVzQUhIr2o9k0Ezzd34Utpzv_6/view?usp=sharing',
             'bttn' => 'btn-secondary',
         ],
         [
             'name' => 'Download Lite from Mega Upload (1.70gb)',
-            'link' => 'https://mega.nz/file/qD4wSA6Z#LEu8xGHqIPhbddaXQOki_KcoILQp3J2rpAwh8CnDgcU',
+            'link' => 'https://mega.nz/file/SOo3BKYS#UDAPIFUWNcfvxbGmQ5Dz_YpC9TqK1plNvjI2zz07zzQ',
             'bttn' => 'btn-secondary',
         ],
         [
             'name' => 'Download Lite from Mediafire (1.70gb)',
-            'link' => 'https://www.mediafire.com/file/55qh9hxt87153ex/Play-Lite.zip/file',
+            'link' => 'https://www.mediafire.com/file/jirxv5gowts0e5o/Play-Lite.zip/file',
             'bttn' => 'btn-secondary',
         ],
     ],
@@ -24,17 +24,17 @@ return [
     'full' => [
         [
             'name' => 'Download Full from Google Drive (3.13gb)',
-            'link' => 'https://drive.google.com/file/d/1Bi-BwISbAEklQ7YA3sehztK-yMCdXKEW/view?usp=sharing',
+            'link' => 'https://drive.google.com/file/d/1WcxfwuFVZPSFlGgDuJWcIoejFzlyeHQh/view?usp=sharing',
             'bttn' => 'btn-primary',
         ],
         [
             'name' => 'Download Full from Mega Upload (3.13gb)',
-            'link' => 'https://mega.nz/file/CChmQD6B#U3NmhBYMwUJi4kPHYMEZhUJckHTgf8K-09HSQKRfmQY',
+            'link' => 'https://mega.nz/file/WGwnkCpB#_7_ZpeHoVEAQtpXu1qmhTAb5GojGa5kvW1i-Q8luQB8',
             'bttn' => 'btn-secondary',
         ],
         [
             'name' => 'Download Full from Mediafire (3.13gb)',
-            'link' => 'https://www.mediafire.com/file/jn5vm0f11ub5tqc/Play.zip/file',
+            'link' => 'https://www.mediafire.com/file/q9pd3wo8g6pkr16/Play.zip/file',
             'bttn' => 'btn-secondary',
         ],
     ],
