971e8b4447ac4d65d6e7f1d7addfdc1136afe129
Mark Hester <Marky360@live.ie>
2022-02-17 23:05:13 +0000

    Update downloads.php

--- config/downloads.php
+++ config/downloads.php
@@ -5,17 +5,17 @@ return [
 
     'lite' => [
         [
-            'name' => 'Download Lite from Google Drive (1.70gb)',
+            'name' => 'Download Lite from Google Drive (2GB)',
             'link' => 'https://drive.google.com/file/d/1CIzmL_HVzQUhIr2o9k0Ezzd34Utpzv_6/view?usp=sharing',
             'bttn' => 'btn-secondary',
         ],
         [
-            'name' => 'Download Lite from Mega Upload (1.70gb)',
+            'name' => 'Download Lite from Mega Upload (2GB)',
             'link' => 'https://mega.nz/file/SOo3BKYS#UDAPIFUWNcfvxbGmQ5Dz_YpC9TqK1plNvjI2zz07zzQ',
             'bttn' => 'btn-secondary',
         ],
         [
-            'name' => 'Download Lite from Mediafire (1.70gb)',
+            'name' => 'Download Lite from Mediafire (2GB)',
             'link' => 'https://www.mediafire.com/file/jirxv5gowts0e5o/Play-Lite.zip/file',
             'bttn' => 'btn-secondary',
         ],
@@ -23,17 +23,17 @@ return [
 
     'full' => [
         [
-            'name' => 'Download Full from Google Drive (3.13gb)',
+            'name' => 'Download Full from Google Drive (3GB)',
             'link' => 'https://drive.google.com/file/d/1WcxfwuFVZPSFlGgDuJWcIoejFzlyeHQh/view?usp=sharing',
             'bttn' => 'btn-primary',
         ],
         [
-            'name' => 'Download Full from Mega Upload (3.13gb)',
+            'name' => 'Download Full from Mega Upload (3GB)',
             'link' => 'https://mega.nz/file/WGwnkCpB#_7_ZpeHoVEAQtpXu1qmhTAb5GojGa5kvW1i-Q8luQB8',
             'bttn' => 'btn-secondary',
         ],
         [
-            'name' => 'Download Full from Mediafire (3.13gb)',
+            'name' => 'Download Full from Mediafire (3GB)',
             'link' => 'https://www.mediafire.com/file/q9pd3wo8g6pkr16/Play.zip/file',
             'bttn' => 'btn-secondary',
         ],
