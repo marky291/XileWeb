143c04ad5bbd55af099b6a23050b3c38c0ca4d38
Mark Hester <marky360@live.ie>
2022-02-20 01:02:09 +0000

    add id to search

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -15,7 +15,7 @@
                 </ul>
                 <div class="flex-1 flex items-center justify-end">
                     <a target="_blank" href="https://wiki.xileretro.net/index.php?search=&title=Special%3ASearch&go=Go">
-                        <button>
+                        <button id="search_button">
                             <svg class="w-6 h-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
                             </svg>
