4ce22056a877c13220c41c5f9cd459ca4fbb230f
Mitsune <50927576+MitsuneBewbs@users.noreply.github.com>
2022-10-02 14:52:32 -0600

    Download Links Updated to 02-10-2022

--- config/downloads.php
+++ config/downloads.php
@@ -5,17 +5,17 @@ return [
     'lite' => [
         [
             'name' => 'Download Lite Client from Google Drive (2GB)',
-            'link' => 'https://drive.google.com/file/d/1CIzmL_HVzQUhIr2o9k0Ezzd34Utpzv_6/view?usp=sharing',
+            'link' => 'https://drive.google.com/file/d/1MEu2rT4AGBt_lgVQrdAGeb2d7G5ShGwR/view?usp=sharing',
             'bttn' => 'btn-primary bg-rose-800 hover:bg-rose-900 text-white',
         ],
         [
             'name' => 'Download Lite Client from Mega Upload (2GB)',
-            'link' => 'https://mega.nz/file/SOo3BKYS#UDAPIFUWNcfvxbGmQ5Dz_YpC9TqK1plNvjI2zz07zzQ',
+            'link' => 'https://mega.nz/file/GPYiET7T#le3fwZSvWusAl0j6uPkObpN3GnobqnGeiJJ8jkxtFUk',
             'bttn' => 'btn-secondary',
         ],
         [
             'name' => 'Download Lite Client from Mediafire (2GB)',
-            'link' => 'https://www.mediafire.com/file/jirxv5gowts0e5o/Play-Lite.zip/file',
+            'link' => 'https://www.mediafire.com/file/ezxcfa4xbcbkgty/Play-Lite.zip/file',
             'bttn' => 'btn-secondary',
         ],
     ],
@@ -23,17 +23,17 @@ return [
     'full' => [
         [
             'name' => 'Download Full Client from Google Drive (3GB)',
-            'link' => 'https://drive.google.com/file/d/1WcxfwuFVZPSFlGgDuJWcIoejFzlyeHQh/view?usp=sharing',
+            'link' => 'https://drive.google.com/file/d/1G0wRDlKBAkwLYqBvxU6aeQFEkVlXhNWH/view?usp=sharing',
             'bttn' => 'btn-primary bg-rose-800 hover:bg-rose-900 text-white',
         ],
         [
             'name' => 'Download Full Client from Mega Upload (3GB)',
-            'link' => 'https://mega.nz/file/WGwnkCpB#_7_ZpeHoVEAQtpXu1qmhTAb5GojGa5kvW1i-Q8luQB8',
+            'link' => 'https://mega.nz/file/OKo3Tb5Y#ErXSCpgxiDKE-j7BspVEKUFT2Ru3bqEFFJXVsF-o6RE',
             'bttn' => 'btn-secondary',
         ],
         [
             'name' => 'Download Full Client from Mediafire (3GB)',
-            'link' => 'https://www.mediafire.com/file/q9pd3wo8g6pkr16/Play.zip/file',
+            'link' => 'https://www.mediafire.com/file/olsww1sajqqewka/Play.zip/file',
             'bttn' => 'btn-secondary',
         ],
     ],
