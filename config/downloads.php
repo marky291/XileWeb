e1bba7bd0606328e90543f6aa100ec5f9eeaa1f8
Mitsune <50927576+MitsuneBewbs@users.noreply.github.com>
2023-08-06 06:02:30 -0600

    Download Links Updated to 06-08-2023

--- config/downloads.php
+++ config/downloads.php
@@ -5,17 +5,17 @@ return [
     'lite' => [
         [
             'name' => 'Download Lite Client from Google Drive (2GB)',
-            'link' => 'https://drive.google.com/file/d/1mIFNtP8PLLrZUuwa9Ws8VUW9j2Q2XdOr/view?usp=sharing',
+            'link' => 'https://drive.google.com/file/d/1NoBQr0VMQs3GQsjvCZG1m2rdouYhTWes/view?usp=sharing',
             'bttn' => 'btn-primary bg-rose-800 hover:bg-rose-900 text-white',
         ],
         [
             'name' => 'Download Lite Client from Mega Upload (2GB)',
-            'link' => 'https://mega.nz/file/zbQynSyJ#zzBX1zjIlXUbJwryeIwCes3AczITZ9r8li5dPr8Maqw',
+            'link' => 'https://mega.nz/file/eP5i2T7Z#8waKqnipWp50ppv626iNsBuD6kC4FCxcBF26U6R2obc',
             'bttn' => 'btn-secondary',
         ],
         [
             'name' => 'Download Lite Client from Mediafire (2GB)',
-            'link' => 'https://www.mediafire.com/file/xdgd3bwclvodtfh/Play-Lite.zip/file',
+            'link' => 'https://www.mediafire.com/file/bvvf60f95p66bxr/XileRetro-Lite.zip/file',
             'bttn' => 'btn-secondary',
         ],
     ],
@@ -23,17 +23,17 @@ return [
     'full' => [
         [
             'name' => 'Download Full Client from Google Drive (3GB)',
-            'link' => 'https://drive.google.com/file/d/1Kf6R_IF6VqSy_ZcqspbqgK0LAFAfA11o/view?usp=sharing',
+            'link' => 'https://drive.google.com/file/d/17xz2zF6och9RMLJKXwqGKIXYwcYA1uaa/view?usp=sharing',
             'bttn' => 'btn-primary bg-rose-800 hover:bg-rose-900 text-white',
         ],
         [
             'name' => 'Download Full Client from Mega Upload (3GB)',
-            'link' => 'https://mega.nz/file/yDRHCLDA#z671HSFPE5UgIZD1h8oDXOvOrVFc2zFBGyBkJNQiBVE',
+            'link' => 'https://mega.nz/file/aCBlEJDS#4WfxRZXUh4908azjzVctX6yXhOx84iVmkcFMCPbWqrg',
             'bttn' => 'btn-secondary',
         ],
         [
             'name' => 'Download Full Client from Mediafire (3GB)',
-            'link' => 'https://www.mediafire.com/file/l0z00alq0xk1zwd/Play.zip/file',
+            'link' => 'https://www.mediafire.com/file/6q56evdx2mxltbx/XileRetro-Full.zip/file',
             'bttn' => 'btn-secondary',
         ],
     ],
