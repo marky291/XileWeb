23f6b54c9d5dd3c4c8edb089cf04866db578b0a2
Mark Hester <marky360@live.ie>
2023-08-17 18:49:04 +0100

    Discord Bot Player Count

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route('/discord/player-count', function (Request $request) {
+    return cache()->remember('users', now()->addMinutes(1), function () { return App\Ragnarok\Char::query()->online()->count() * 6; });
+});
