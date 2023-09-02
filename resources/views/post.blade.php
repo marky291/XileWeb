bac8d462801b2cf3f85a9a20f1d911921c359ee2
Mark Hester <marky360@live.ie>
2023-09-02 14:20:21 +0100

    Title urls for posts

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,15 @@
 <x-app-layout>
 
-    @section('title', 'XileRO | War of Emperium')
-    @section('description', "Join the War of Emperium on XileRO. Engage in epic castle battles, explore real-time ownership, and find battle schedules across all timezones. Answer the call to glory today!")
-    @section('keywords', 'Ragnarok Online, War of Emperium, Xilero, castle battles, private server, WoE times, MMO, online gaming, guild wars, PvP, real-time strategy')
+    @section("title", "XileRO | Update: " . $post->title)
+    @section('description', $post->blurb)
+    @section('keywords', 'XileRO, Updates, Changelog')
 
     <section class="shadow bg-[url('../assets/landing-sitting.jpeg')] bg-cover md:pt-16">
         <div class="container grid px-3 sm:px-0 grid-cols-5 gap-4 mx-auto">
             <div class="col-span-5 pt-20 pb-4">
                 <div class="p-8 rounded bg-zinc-900/90">
                     <div class="prose text-gray-300 tracking-normal text-lg">
+                        <p class="text-amber-500 mb-4">{{ $post->created_at->diffForHumans() }}</p>
                         <h1 class="mb-6 text-white text-3xl">{{ $post->title }}</h1>
 
                         <x-markdown class="markdown text-gray-100">
