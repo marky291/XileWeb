4f2c3ccf1125e69f7094325b38804e779ae37812
Mark Hester <marky360@live.ie>
2023-09-15 16:21:29 +0100

    Hourly job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,11 +18,11 @@ class Kernel extends ConsoleKernel
         //$schedule->command('inspire')->everyFiveSeconds();
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
 
-        $schedule->job(new PostGuildPointsToDiscordJob('Kriemhild', today()))->hourly()->withoutOverlapping();
-        $schedule->job(new PostGuildPointsToDiscordJob('Swanhild', today()))->hourly()->withoutOverlapping();
-        $schedule->job(new PostGuildPointsToDiscordJob('Fadhringh', today()))->hourly()->withoutOverlapping();
-        $schedule->job(new PostGuildPointsToDiscordJob('Skoegul', today()))->hourly()->withoutOverlapping();
-        $schedule->job(new PostGuildPointsToDiscordJob('Gondul', today()))->hourly()->withoutOverlapping();
+        $schedule->job(new PostGuildPointsToDiscordJob('Kriemhild', today()))->hourlyAt(5)->withoutOverlapping();
+        $schedule->job(new PostGuildPointsToDiscordJob('Swanhild', today()))->hourlyAt(5)->withoutOverlapping();
+        $schedule->job(new PostGuildPointsToDiscordJob('Fadhringh', today()))->hourlyAt(5)->withoutOverlapping();
+        $schedule->job(new PostGuildPointsToDiscordJob('Skoegul', today()))->hourlyAt(5)->withoutOverlapping();
+        $schedule->job(new PostGuildPointsToDiscordJob('Gondul', today()))->hourlyAt(5)->withoutOverlapping();
     }
 
     /**
