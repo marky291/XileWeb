26b013e83c14e0ffc57cc0a7248974c02c641853
Mitsune <50927576+MitsuneBewbs@users.noreply.github.com>
2023-10-02 23:31:17 -0600

    10-02-2023 Full & Android Links Updated

--- config/downloads.php
+++ config/downloads.php
@@ -13,34 +13,31 @@ return [
             'link' => 'https://mega.nz/file/zbQynSyJ#zzBX1zjIlXUbJwryeIwCes3AczITZ9r8li5dPr8Maqw',
             'bttn' => 'btn-secondary bg-amber-500 hover:bg-amber-300',
         ],
-        [
-            'name' => 'Download Lite Client from Mediafire (2GB)',
-            'link' => 'https://www.mediafire.com/file/xdgd3bwclvodtfh/Play-Lite.zip/file',
-            'bttn' => 'btn-secondary bg-amber-500 hover:bg-amber-300',
-        ],
     ],
 
     'full' => [
         [
-            'name' => 'Download Full Client from Google Drive (3GB)',
-            'link' => 'https://drive.google.com/file/d/1Kf6R_IF6VqSy_ZcqspbqgK0LAFAfA11o/view?usp=sharing',
+            'name' => 'FULL CLIENT FROM GOOGLE DRIVE (3GB)',
+            'link' => 'https://drive.google.com/file/d/15xWYVMxvpqOHZl8qb-G25_zcuKt9k8Pl/view?usp=sharing',
             'bttn' => 'btn-primary bg-amber-300 hover:bg-amber-100 text-gray-900',
         ],
         [
-            'name' => 'Download Full Client from Mega Upload (3GB)',
-            'link' => 'https://mega.nz/file/yDRHCLDA#z671HSFPE5UgIZD1h8oDXOvOrVFc2zFBGyBkJNQiBVE',
-            'bttn' => 'btn-secondary bg-amber-500 hover:bg-amber-300',
-        ],
-        [
-            'name' => 'Download Full Client from Mediafire (3GB)',
-            'link' => 'https://www.mediafire.com/file/l0z00alq0xk1zwd/Play.zip/file',
+            'name' => 'FULL CLIENT FROM MEGA UPLOAD (3GB)',
+            'link' => 'https://mega.nz/file/OWIwFKrL#X9iuqN5GiRTxkoTJz3z8oC_u3ngtlzvovrDcTVRAcvM',
             'bttn' => 'btn-secondary bg-amber-500 hover:bg-amber-300',
         ],
     ],
 
     'android' => [
-        'title' => 'Download Android',
-        'link' => 'http://roworkshop.com/apk/315/XileRetro.apk',
-        'version' => '3.15',
+        'name' => 'ANDROID FROM GOOGLE DRIVE (3MB)',
+            'link' => 'https://drive.google.com/file/d/10hgx13x5PnSWk_u7GTIucbFcWS1asPjg/view?usp=sharing',
+            'bttn' => 'btn-primary bg-amber-300 hover:bg-amber-100 text-gray-900',
+            'version' => 'v458',
+        ],
+        [
+           'name' => 'ANDROID FROM MEGA UPLOAD (3MB)',
+           'link' => 'https://mega.nz/file/yWwBnJaC#sKEHPQRw6v16QbxB8VcP2u8TmCzjuQqlwIYbtsIBeOc',
+           'bttn' => 'btn-secondary bg-amber-500 hover:bg-amber-300',
+           'version' => 'v458',
     ],
 ];
