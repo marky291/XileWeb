ad7b9b4e6b583972c8acfb9073eb11a06ae5cf69
Mark Hester <marky360@live.ie>
2024-06-18 21:30:33 +0100

    Readable patch note.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,10 +4,10 @@
     @section('description', $post->blurb)
     @section('keywords', 'XileRO, Updates, Changelog')
 
-    <section class="shadow bg-[url('../assets/landing-sitting.jpeg')] bg-cover md:pt-16">
+    <section class="shadow md:pt-16 bg-gray-800">
         <div class="container grid px-3 sm:px-0 grid-cols-5 gap-4 mx-auto">
-            <div class="col-span-5 pt-20 pb-4">
-                <div class="p-8 rounded bg-zinc-900/90">
+            <div class="col-span-5 pt-20 pb-4 bg-gray-900">
+                <div class="p-8 rounded">
                     <div class="prose text-gray-300 tracking-normal text-lg">
                         <p class="text-amber-500 mb-4">{{ $post->created_at->diffForHumans() }}</p>
                         <h1 class="mb-6 text-white text-3xl">{{ $post->title }}</h1>
