323c45103559ad94b40f532126bb786ab9882d95
Mark Hester <marky360@live.ie>
2025-08-29 17:08:05 +0100

    100mb upload

--- config/livewire.php
+++ config/livewire.php
@@ -53,7 +53,7 @@ return [
 
     'temporary_file_upload' => [
         'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
-        'rules' => ['required', 'file', 'max:51200'],       // 50MB limit for patch files
+        'rules' => ['required', 'file', 'max:102400'],       // 100MB limit for patch files
         'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
         'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
