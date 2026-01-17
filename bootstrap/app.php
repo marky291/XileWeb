4af953d14acc733821537f07b2bdb669bfe33612
Mark Hester <marky360@live.ie>
2026-01-17 13:23:34 +0000

    log down game server

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +16,18 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->report(function (QueryException $e) {
+            $connection = $e->getConnectionName();
+            $gameConnections = ['xilero_main', 'xilero_logs', 'xileretro_main', 'xileretro_logs'];
+
+            if (in_array($connection, $gameConnections)) {
+                Log::channel('single')->error("Game database unavailable: {$connection}", [
+                    'connection' => $connection,
+                    'error' => $e->getMessage(),
+                    'code' => $e->getCode(),
+                ]);
+            }
+        });
     })
     ->withSchedule(function ($schedule) {
         //
