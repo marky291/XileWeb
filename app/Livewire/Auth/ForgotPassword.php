a2138e9294f2e6d8297794482beabb9f3f4764ba
Mark Hester <marky360@live.ie>
2026-01-17 13:51:42 +0000

    show email throttle

--- app/Livewire/Auth/ForgotPassword.php
+++ app/Livewire/Auth/ForgotPassword.php
@@ -29,6 +29,8 @@ class ForgotPassword extends Component
         if ($status === Password::RESET_LINK_SENT) {
             $this->emailSent = true;
             session()->flash('status', __($status));
+        } elseif ($status === Password::RESET_THROTTLED) {
+            $this->addError('email', 'Please wait 60 seconds before requesting another reset link.');
         } else {
             $this->addError('email', __($status));
         }
