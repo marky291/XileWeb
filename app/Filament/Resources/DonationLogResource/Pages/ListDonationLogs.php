753a1fb7cd79eec54e1f216ba759d340ff1e8126
Mark Hester <marky360@live.ie>
2026-01-22 03:03:00 +0000

    fix

--- app/Filament/Resources/DonationLogResource/Pages/ListDonationLogs.php
+++ app/Filament/Resources/DonationLogResource/Pages/ListDonationLogs.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\DonationLogResource\Pages;
 
 use App\Filament\Resources\DonationLogResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListDonationLogs extends ListRecords
 {
     protected static string $resource = DonationLogResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-        ];
-    }
 }
